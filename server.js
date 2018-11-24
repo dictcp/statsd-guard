162776eec0c147d06f1ac0258a59cb56f9f82460
Dick Tang <dick.tang@ymail.com>
2018-11-24 23:38:01 +0800

    fix typo; support startWith string matching

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const client = dgram.createSocket('udp4');
 var argv = require('minimist')(process.argv.slice(2));
 
 // tested with docker run -it -p 8125:8125/udp dictcp/utils socat udp-recvfrom:8125,fork stdout
-const filter_regex = [
+const filter_rules = [
+  "test3",
   /^testing/,
   /^test2/
 ];
@@ -14,15 +15,15 @@ server.on('message', (msg, rinfo) => {
   // console.log(`server got: message with len ${msg.length} from ${rinfo.address}:${rinfo.port}`);
   
   var metric_array = msg.toString('ascii').split("\n");
-  metric_array = metric_array.filter((x) => filter_regex.some((r) => r.test(x)));
+  metric_array = metric_array.filter((x) => filter_rules.some((r) => (r instanceof RegExp) ? r.test(x) : x.startsWith(r)));
 
-  if (metric_array.count>0) {
+  if (metric_array.length > 0) {
     const message = Buffer.from(metric_array.join("\n") + "\n");
 
     // console.log(`server send: message with len ${message.length} to ${argv.chost}:${argv.cport}`);
 
     client.send(message, parseInt(argv.cport), argv.chost, (err) => {
-      client.close();
+      // client.close();
     });
   }
 });
